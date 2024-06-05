c629ee3d74c32da55aee3e6190871231a672745a
qiqzhao <bme_ritter@foxmail.com>
2024-06-05 10:53:23 +0800

    feat: handle no data

--- src/pages/profile/detail.jsx
+++ src/pages/profile/detail.jsx
@@ -44,6 +44,8 @@ const OperationPanel = styled.div`
   text-align: center;
 `;
 
+const USER_ID = '665ee30d488e7c70da14d48b';
+
 export const ProfileDetail = () => {
   const [user, setUser] = useState();
 
@@ -52,42 +54,48 @@ export const ProfileDetail = () => {
       const data = await getUser(id);
       setUser(data);
     }
-    getUserInfo('665ee30d488e7c70da14d48b');
+    getUserInfo(USER_ID);
   }, []);
 
   return (
     <Container>
-      <Avatar size={150} src={user?.avatar} />
+      {!!user ? (
+        <>
+          <Avatar size={150} src={user?.avatar} />
 
-      <NameLine>
-        <h3>{user?.name}</h3>
-        <StyledTag color={ROLE_LABEL_MAP[user?.role || 'FRONTEND']}>{user?.role}</StyledTag>
-      </NameLine>
+          <NameLine>
+            <h3>{user?.name}</h3>
+            <StyledTag color={ROLE_LABEL_MAP[user?.role || 'FRONTEND']}>{user?.role}</StyledTag>
+          </NameLine>
 
-      <Detail>
-        <InformationLine>
-          <span>Country: </span>
-          <strong>{user?.country}</strong>
-        </InformationLine>
-        <InformationLine>
-          <span>City: </span>
-          <strong>{user?.city}</strong>
-        </InformationLine>
-        <InformationLine>
-          <span>Phone: </span>
-          <strong>{user?.phone}</strong>
-        </InformationLine>
-        <InformationLine>
-          <span>Email: </span>
-          <strong>{user?.email}</strong>
-        </InformationLine>
-      </Detail>
+          <Detail>
+            <InformationLine>
+              <span>Country: </span>
+              <strong>{user?.country}</strong>
+            </InformationLine>
+            <InformationLine>
+              <span>City: </span>
+              <strong>{user?.city}</strong>
+            </InformationLine>
+            <InformationLine>
+              <span>Phone: </span>
+              <strong>{user?.phone}</strong>
+            </InformationLine>
+            <InformationLine>
+              <span>Email: </span>
+              <strong>{user?.email}</strong>
+            </InformationLine>
+          </Detail>
 
-      <OperationPanel>
-        <Button type="primary">
-          <Link to={`/profile/edit/${user?._id}`}>Edit Profile</Link>
-        </Button>
-      </OperationPanel>
+          <OperationPanel>
+            <Button type="primary">
+              <Link to={`/profile/edit/${user?._id}`}>Edit Profile</Link>
+            </Button>
+          </OperationPanel>
+        </>
+      ) : (
+        <div>No user</div>
+      )}
     </Container>
   );
 };
